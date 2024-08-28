61e5d74ab57db1f96519d26f39d2154dd2076e07
shahviraj222 <viraj22feb@gmail.com>
2024-08-28 19:45:02 +0530

    statically the folder can serve by the server

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,9 @@ import cors from 'cors'
 import express from 'express'
 
 const app = express()
+app.use(express.static('dist'))
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 export { app }
